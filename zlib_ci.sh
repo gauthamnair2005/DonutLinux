12ff7c70f99144bd444c81c0fe9fab130b8b637c
Gautham Nair <gautham.nair.2005@outlook.com>
2023-03-15 21:20:13 +0530

    Update zlib_ci.sh

--- zlib_ci.sh
+++ zlib_ci.sh
@@ -1,5 +1,1 @@
-sudo tar -xvf zlib-1.2.13.tar.xz
-cd zlib-1.2.12
-sudo ./configure
-sudo make
-sudo make install
+sudo tar -xvf zlib-1.2.13.tar.xz && cd zlib-1.2.13 && sudo ./configure && sudo make && sudo make install
