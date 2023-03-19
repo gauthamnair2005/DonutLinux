2babfc01133f627126d1e05bf19a85275d4bc1af
Gautham Nair <gautham.nair.2005@outlook.com>
2023-03-19 18:53:26 +0530

    Update version-check.sh

--- version-check.sh
+++ version-check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Simple script to list version numbers of critical development tools required for DonutOS/DonutLinux development
+# Simple script to list version numbers of critical development tools
 export LC_ALL=C
 bash --version | head -n1 | cut -d" " -f2-4
 MYSH=$(readlink -f /bin/sh)
 echo "/bin/sh -> $MYSH"
-echo $MYSH | grep -q bash || echo "ERROR: /bin/sh does not point towards bash"
+echo $MYSH | grep -q bash || echo "ERROR: /bin/sh does not point to bash"
 unset MYSH
 
 echo -n "Binutils: "; ld --version | head -n1 | cut -d" " -f3-
@@ -43,11 +43,11 @@ echo Perl `perl -V:version`
 python3 --version
 sed --version | head -n1
 tar --version | head -n1
-makeinfo --version | head -n1
+makeinfo --version | head -n1  # texinfo version
 xz --version | head -n1
 
 echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
 if [ -x dummy ]
-  then echo "G++ Compilation OK";
-  else echo "G++ Compilation Failed"; fi
+  then echo "g++ compilation OK";
+  else echo "g++ compilation failed"; fi
 rm -f dummy.c dummy
